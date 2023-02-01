1ea74fb2359f2a2d13549c5ae7f69cdb9952488c
Wouter van Wageningen <wouter.vdub@yahoo.com>
2023-02-01 23:31:00 +0000

    Plugin Arrow fix

--- plugins/plugin-pleat/src/index.mjs
+++ plugins/plugin-pleat/src/index.mjs
@@ -1,8 +1,8 @@
 import { name, version } from '../data.mjs'
 
 const markers = `
-<marker orient="auto" refY="4.0" refX="12.0" id="pleatTo" style="overflow:visible;" markerWidth="12" markerHeight="8">
-	<path class="note fill-note" d="M 12,4 L 0,0 C 2,2 2,6  0,8 z" />
+<marker id="pleatTo" markerWidth="12" markerHeight="8" orient="auto" refY="4" refX="12">
+  <path class="note fill-note" d="M 12,4 L 0,0 C 2,2 2,6  0,8 z" />
 </marker>
 `
 
@@ -24,11 +24,10 @@ export const plugin = {
         delete paths.pleatTo
         delete paths.pleatFrom
         delete paths.pleatArrow
-        // setCutOnFold relies on plugin-cutlist
         return true
       }
       so = {
-        margin: 15,
+        margin: 35,
         prefix: 'pleat',
         reverse: false,
         ...so,
@@ -37,27 +36,36 @@ export const plugin = {
         points[so.prefix + 'From'] = so.from
         points[so.prefix + 'To'] = so.to
         points[so.prefix + 'FromIn'] = points[so.prefix + 'From'].shift(
-          so.from.shiftTowards(so.to, 0.1).angle(so.from) + 90,
+          so.from.shiftTowards(so.to, 0.1).angle(so.from) + 270,
           so.margin * scale
         )
         points[so.prefix + 'ToIn'] = points[so.prefix + 'To'].shift(
-          so.from.shiftTowards(so.to, 0.1).angle(so.to) + 270,
+          so.from.shiftTowards(so.to, 0.1).angle(so.to) + 90,
           so.margin * scale
         )
         paths[so.prefix + 'PleatFrom'] = new Path()
           .move(points[so.prefix + 'From'])
           .line(points[so.prefix + 'FromIn'])
-          .attr('class', 'note ' + (so.reverse ? 'dotted' : ''))
+          .attr('class', 'note ' + (so.reverse ? 'dashed' : ''))
         paths[so.prefix + 'PleatTo'] = new Path()
           .move(points[so.prefix + 'To'])
           .line(points[so.prefix + 'ToIn'])
-          .attr('class', 'note ' + (so.reverse ? '' : 'dotted'))
+          .attr('class', 'note ' + (so.reverse ? '' : 'dashed'))
         paths[so.prefix + 'PleatArrow'] = new Path()
-          .move(points[so.prefix + 'From'].shiftFractionTowards(points[so.prefix + 'FromIn'], 0.25))
-          .line(points[so.prefix + 'To'].shiftFractionTowards(points[so.prefix + 'ToIn'], 0.25))
+          .move(
+            points[so.prefix + (so.reverse ? 'To' : 'From')].shiftFractionTowards(
+              points[so.prefix + (so.reverse ? 'ToIn' : 'FromIn')],
+              0.25
+            )
+          )
+          .line(
+            points[so.prefix + (so.reverse ? 'From' : 'To')].shiftFractionTowards(
+              points[so.prefix + (so.reverse ? 'FromIn' : 'ToIn')],
+              0.25
+            )
+          )
           .attr('class', 'note')
-          .attr('marker-start', so.reverse ? 'url(#pleatTo)' : '')
-          .attr('marker-end', so.reverse ? '' : 'url(#pleatTo)')
+          .attr('marker-end', 'url(#pleatTo)')
       }
     },
   },
