58cce8cf86c04c41ef96dc4c0a5ead294ffafa28
Wouter van Wageningen <wouter.vdub@yahoo.com>
2023-02-13 19:06:04 +0000

    Front Pocket Facing

---
+++ designs/zoot/src/front-pocket-facing.mjs
@@ -0,0 +1,30 @@
+import { frontPocketFacing as charlieFrontPocketFacing } from '@freesewing/charlie'
+
+function draftZootFrontPocketFacing({
+  points,
+  Point,
+  paths,
+  Path,
+  complete,
+  macro,
+  snippets,
+  sa,
+  part,
+}) {
+  for (let id in snippets) delete snippets[id]
+
+  if (complete) {
+    macro('sprinkle', {
+      snippet: 'notch',
+      on: ['slantTop', 'slantCurveEnd'],
+    })
+  }
+
+  return part
+}
+
+export const frontPocketFacing = {
+  name: 'zoot.frontPocketFacing',
+  from: charlieFrontPocketFacing,
+  draft: draftZootFrontPocketFacing,
+}
